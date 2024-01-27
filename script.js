6e95363364924afbc221c722cf01a2363a6bde7a
Freddie Heath <fheath090401@gmail.com>
2024-01-27 12:54:50 +0000

    Made all citation components conditional through IF statements and an array method

--- script.js
+++ script.js
@@ -1,60 +1,64 @@
-// To-do
-// Italicise title in bib ref
 // 'Today' date option
 // Multiple contributors
 // Multiple references (append a list)
 // Clear list
-// Keep list after refresh
-// Formatting of list
+// Alphabetice list
+// Keep list after refresh?
+// Access Date
 
-const firstName = document.getElementById("firstName");
 const lastName = document.getElementById("lastName");
+const firstName = document.getElementById("firstName");
 const title = document.getElementById("title");
 const url = document.getElementById("URL");
 const year = document.getElementById("year");
 const publisher = document.getElementById("publisher");
+const quote = document.getElementById("quote");
 const button = document.getElementById("button");
-const textRef = document.getElementById("inTextReference");
-const bibRef = document.getElementById("bibliographyReference");
+const textRefList = document.getElementById("inTextReferenceList");
+const bibRefList = document.getElementById("bibliographyReferenceList");
+
+function addContributor() {}
 
 function printBibRef() {
-  if (
-    lastName.value &&
-    firstName.value &&
-    year.value &&
-    title.value &&
-    publisher.value &&
-    url.value
-  ) {
-    bibRef.innerHTML =
-      "Bibliography Citation: " +
-      lastName.value +
-      ", " +
-      firstName.value[0] +
-      ". " +
-      "(" +
-      year.value +
-      "). " +
-      "<i>" +
-      title.value +
-      "</i>" +
-      ". [online] " +
-      publisher.value +
-      ". " +
-      "Available at: " +
-      url.value +
-      ".";
-  } else {
-    alert("Please fill in all fields before submitting");
+  const citationComponents = [];
+
+  if (lastName.value) {
+    citationComponents.push(lastName.value + ", ");
   }
+
+  if (firstName.value) {
+    citationComponents.push(firstName.value[0] + ". ");
+  }
+
+  if (year.value) {
+    citationComponents.push("(" + year.value + "). ");
+  }
+
+  if (title.value) {
+    citationComponents.push("<i>" + title.value + "</i>" + ". [online] ");
+  }
+
+  if (publisher.value) {
+    citationComponents.push(publisher.value);
+  }
+
+  if (url.value) {
+    citationComponents.push(" Available at: " + url.value + ". ");
+  }
+
+  if (quote.value) {
+    citationComponents.push(' "' + "<i>" + quote.value + "</i>" + '"');
+  }
+
+  bibRefList.innerHTML = citationComponents.join("") + ".";
 }
 
 function printTextRef() {
-  if (lastName.value && year.value) {
-    textRef.textContent =
-      "In-text citation: " + "(" + lastName.value + ", " + year.value + ")";
-  }
+  textRefList.innerHTML =
+    "In-Text Citation: " + "(" + lastName.value + ", " + year.value + ")";
 }
 
-button.addEventListener("click", printBibRef);
-button.addEventListener("click", printTextRef);
+button.addEventListener("click", () => {
+  printBibRef();
+  printTextRef();
+});
